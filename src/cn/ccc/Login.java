90bcc88ac09480babdf2393be474af6b486fee1d
trank <mr.cbb@outlook.com>
2017-12-13 20:46:29 +0800

    second test

---
+++ src/cn/ccc/Login.java
@@ -0,0 +1,26 @@
+package cn.ccc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by cuibinbin on 2017/10/13.
+ */
+public class Login extends HttpServlet{
+    public static void main(String[] args){
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
